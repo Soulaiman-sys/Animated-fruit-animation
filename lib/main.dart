095d2350a43ae51dc676428ae4ffc890fb673da4
soulaiman laghmich <laghmich.soulaiman@gmail.com>
2025-05-28 22:01:20 +0100

    init project

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_animation/constants.dart';
+import 'package:fruit_animation/screens/home/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: TextButton.styleFrom(
+            padding: EdgeInsets.all(defaultPadding * 0.75),
+            shape: StadiumBorder(),
+            backgroundColor: primaryColor,
+          ),
+        ),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
